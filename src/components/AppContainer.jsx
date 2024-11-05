a0ed28fd91ec1e998ceec58ff206362d842eb00b
EliasSilva1702 <eliassilva170206@gmail.com>
2024-11-05 09:06:26 -0300

    "commit"

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -4,7 +4,7 @@ import { Toaster } from "react-hot-toast";
 function AppContainer({ children }) {
   return (
     <main className="border min-h-screen background-gradient-clouds font-moderustic flex place-items-center">
-      <div className="h-full w-fit mx-auto backdrop-blur-lg bg-white/30 my-4 md:my-8 p-4 md:p-32 rounded-xl shadow-2xl">
+      <div className="h-full w-fit mx-auto backdrop-blur-lg bg-white/30 my-4 md:my-8 p-4 md:p-24 rounded-xl shadow-2xl">
         {children}
       </div>
       <Toaster position="bottom-right" reverseOrder={false} />
